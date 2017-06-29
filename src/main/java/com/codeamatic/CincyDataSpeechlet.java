4baac7491153a4863d320c797059525f6f84cdb9
Jason Webb <jason@jswebb.net>
2017-06-29 12:44:43 -0400

    Added a neighborhood support intent for listing off neighborhoods

--- src/main/java/com/codeamatic/CincyDataSpeechlet.java
+++ src/main/java/com/codeamatic/CincyDataSpeechlet.java
@@ -16,13 +16,21 @@ import com.amazon.speech.ui.SimpleCard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.amazon.speech.speechlet.SpeechletResponse.newAskResponse;
+
 /**
  * Cinci Data Speechlet for handling Alexa Speechlet requests.
  */
 public class CincyDataSpeechlet implements Speechlet {
   private static final Logger log = LoggerFactory.getLogger(CincyDataSpeechlet.class);
 
+  private static final String SOCRATA_KEY = System.getenv("SOCRATA_KEY");
+  private static final String SOCRATA_SECRET = System.getenv("SOCRATA_SECRET");
   private static final String SKILL_NAME = "Cincy Data";
+
+  private static final String SLOT_NEIGHBORHOOD = "neighborhood";
+  private static final String SLOT_DATE = "date";
+  private static final String SLOT_DATE_STRING = "date_string";
 
   @Override
   public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
@@ -45,13 +53,15 @@ public class CincyDataSpeechlet implements Speechlet {
 
     switch(intentName) {
       case "CrimeReportIntent":
-        return getWelcomeResponse();
+        return getCrimeReportResponse();
+      case "SupportedNeighborhoodsIntent":
+        return getSupportedNeighborhoodsResponse();
       case "AMAZON.HelpIntent":
-        return getWelcomeResponse();
+        return getHelpResponse();
       case "AMAZON.StopIntent":
-        return getWelcomeResponse();
+        return getStopResponse();
       case "AMAZON.CancelIntent":
-        return getWelcomeResponse();
+        return null;
       default:
         throw new SpeechletException("Invalid Intent");
     }
@@ -68,6 +78,48 @@ public class CincyDataSpeechlet implements Speechlet {
    * @return SpeechletResponse spoken and visual response for the given intent
    */
   private SpeechletResponse getWelcomeResponse() {
+    String neighborhoodPrompt = "Which neighborhood would you like a crime report for?";
+    String speechText = "Welcome to the " + SKILL_NAME + " skill. "
+            + "You can get crime and incident data for neighborhoods in the city of Cincinnati. "
+            + "For example, you can say, give me yesterday's crime report for Avondale. "
+            + "For a list of supported neighborhoods, ask what neighborhoods are supported. "
+            + "For additional instructions on what you can say, say help me. "
+            + neighborhoodPrompt;
+
+    // Create the Simple card content
+    SimpleCard card = new SimpleCard();
+    card.setTitle("Cincy Data");
+    card.setContent(speechText);
+
+    // Create the plain text output.
+    PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+    speech.setText(neighborhoodPrompt);
+
+    // Create reprompt
+    Reprompt reprompt = new Reprompt();
+    reprompt.setOutputSpeech(speech);
+
+    return newAskResponse(speech, reprompt, card);
+  }
+
+  /**
+   * Creates and returns a {@code SpeechletResponse} for when a user would like the skill to stop.
+   *
+   * @return SpeechletResponse spoken response for the given intent
+   */
+  private SpeechletResponse getStopResponse() {
+    PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+    speech.setText("Goodbye!");
+
+    return SpeechletResponse.newTellResponse(speech);
+  }
+
+  /**
+   * Creates and returns a {@code SpeechletResponse} with a welcome message and card.
+   *
+   * @return SpeechletResponse spoken and visual response for the given intent
+   */
+  private SpeechletResponse getHelpResponse() {
     String speechText = "Welcome to the " + SKILL_NAME + " skill."
             + " You can get crime and incident data for the city of Cincinnati."
             + " For example, you could say, give me a crime report."
@@ -86,6 +138,55 @@ public class CincyDataSpeechlet implements Speechlet {
     Reprompt reprompt = new Reprompt();
     reprompt.setOutputSpeech(speech);
 
-    return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    return newAskResponse(speech, reprompt, card);
+  }
+
+  private SpeechletResponse getCrimeReportResponse() {
+    return null;
+  }
+
+  /**
+   * Creates and returns a {@code SpeechletResponse} with a list of neighborhoods and card.
+   *
+   * @return SpeechletResponse spoken and visual response for the given intent
+   */
+  private SpeechletResponse getSupportedNeighborhoodsResponse() {
+    String repromptText = "Which neighborhood would you like a crime report for?";
+    String speechOutputText = "Currently, I have crime and incident data for the following Cincinnati neighborhoods: "
+            + getAllNeighborhoods() + repromptText;
+
+    // Card
+    SimpleCard card = new SimpleCard();
+    card.setTitle("Cincy Data - Neighborhoods");
+    card.setContent(speechOutputText);
+
+    // Plaintext Original Speech
+    PlainTextOutputSpeech speechOutput = new PlainTextOutputSpeech();
+    speechOutput.setText(speechOutputText);
+
+    // Plaintext Reprompt Speech
+    PlainTextOutputSpeech repromptSpeechOutput = new PlainTextOutputSpeech();
+    repromptSpeechOutput.setText(repromptText);
+
+    Reprompt repromptOutput = new Reprompt();
+    repromptOutput.setOutputSpeech(repromptSpeechOutput);
+
+    return newAskResponse(speechOutput, repromptOutput, card);
+  }
+
+  /**
+   * Retrieve a listing of all of the supported neighborhoods, in a speech friendly format.
+   *
+   * @return String list of all supported neighborhoods
+   */
+  private String getAllNeighborhoods() {
+    StringBuilder neighborhoodsList = new StringBuilder();
+
+    for(String neighborhood : Neighborhoods.getNeighborhoods()) {
+      neighborhoodsList.append(neighborhood);
+      neighborhoodsList.append(", ");
+    }
+
+    return neighborhoodsList.toString();
   }
 }
---
+++ src/main/java/com/codeamatic/Neighborhoods.java
@@ -0,0 +1,74 @@
+package com.codeamatic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Neighborhoods {
+
+  private static final List<String> neighborhoods = new ArrayList<String>();
+
+  private Neighborhoods() {}
+
+  static {
+    neighborhoods.add("Avondale");
+    neighborhoods.add("Bond Hill");
+    neighborhoods.add("Riverfront");
+    neighborhoods.add("CBD");
+    neighborhoods.add("California");
+    neighborhoods.add("Camp Washington");
+    neighborhoods.add("Carthage");
+    neighborhoods.add("Clifton");
+    neighborhoods.add("University Heights");
+    neighborhoods.add("College Hill");
+    neighborhoods.add("Columbia Tusculum");
+    neighborhoods.add("Corryville");
+    neighborhoods.add("CUF");
+    neighborhoods.add("Downtown");
+    neighborhoods.add("East End");
+    neighborhoods.add("East Price Hill");
+    neighborhoods.add("East Walnut Hills");
+    neighborhoods.add("East Westwood");
+    neighborhoods.add("English Woods");
+    neighborhoods.add("Evanston");
+    neighborhoods.add("Fairview");
+    neighborhoods.add("Hartwell");
+    neighborhoods.add("Hyde Park");
+    neighborhoods.add("Kennedy Heights");
+    neighborhoods.add("Linwood");
+    neighborhoods.add("Lower Price Hill");
+    neighborhoods.add("Madisonville");
+    neighborhoods.add("Millvale");
+    neighborhoods.add("Mount Adams");
+    neighborhoods.add("Mount Airy");
+    neighborhoods.add("Mount Auburn");
+    neighborhoods.add("Mount Lookout");
+    neighborhoods.add("Mount Washington");
+    neighborhoods.add("North Avondale");
+    neighborhoods.add("Paddock Hills");
+    neighborhoods.add("North Fairmount");
+    neighborhoods.add("Northside");
+    neighborhoods.add("O'Bryonville");
+    neighborhoods.add("Oakley");
+    neighborhoods.add("Over-The-Rhine");
+    neighborhoods.add("Pendleton");
+    neighborhoods.add("Pleasant Ridge");
+    neighborhoods.add("Queensgate");
+    neighborhoods.add("Riverside");
+    neighborhoods.add("Roselawn");
+    neighborhoods.add("Sayler Park");
+    neighborhoods.add("Sedamsville");
+    neighborhoods.add("South Cumminsville");
+    neighborhoods.add("South Fairmount");
+    neighborhoods.add("Spring Grove Village");
+    neighborhoods.add("Villages at Roll Hill");
+    neighborhoods.add("Walnut Hills");
+    neighborhoods.add("West End");
+    neighborhoods.add("West Price Hill");
+    neighborhoods.add("Westwood");
+    neighborhoods.add("Winton Hills");
+  }
+
+  public static List<String> getNeighborhoods() {
+    return neighborhoods;
+  }
+}
